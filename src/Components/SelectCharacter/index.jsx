8a5050e146d6c652722475e41a1ec0343002032b
EddyJens1 <21762424-EddyJens1@users.noreply.replit.com>
2023-05-11 02:32:09 +0000

    Initial commit

---
+++ src/Components/SelectCharacter/index.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from "react";
+import "./SelectCharacter.css";
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, transformCharacterData } from "../../constants";
+import myEpicGame from "../../utils/MyEpicGame.json";
+
+
+const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+  const [mintingCharacter, setMintingCharacter] = useState(false);
+
+  const mintCharacterNFTAction = (characterId) => async () => {
+    try {
+      if (gameContract) {
+        setMintingCharacter(true);
+        console.log("Mintando personagem...");
+        const mintTxn = await gameContract.mintCharacterNFT(characterId);
+        await mintTxn.wait();
+        console.log(mintTxn);
+        setMintingCharacter(false);
+      }
+    } catch (error) {
+      console.warn("Ação de mintar com erro: ", error);
+      setMintingCharacter(false);
+    }
+  };
+
+  useEffect(() => {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicGame.abi,
+        signer
+      );
+
+      setGameContract(gameContract);
+    } else {
+      console.log("Objeto Ethereum não encontrado");
+    }
+  }, []);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        console.log("Trazendo personagens do contrato para mintar");
+
+        const charactersTxn = await gameContract.getAllDefaultCharacters();
+        console.log("charactersTxn:", charactersTxn);
+
+        const characters = charactersTxn.map((characterData) =>
+          transformCharacterData(characterData)
+        );
+
+        setCharacters(characters);
+      } catch (error) {
+        console.error("Algo deu errado ao trazer personagens:", error);
+      }
+    };
+
+    const onCharacterMint = async (sender, tokenId, characterIndex) => {
+      console.log(
+        `CharacterNFTMinted - sender: ${sender} tokenId: ${tokenId.toNumber()} characterIndex: ${characterIndex.toNumber()}`
+      );
+
+      if (gameContract) {
+        const characterNFT = await gameContract.checkIfUserHasNFT();
+        console.log("CharacterNFT: ", characterNFT);
+        setCharacterNFT(transformCharacterData(characterNFT));
+      }
+    };
+
+    if (gameContract) {
+      getCharacters();
+
+      gameContract.on("CharacterNFTMinted", onCharacterMint);
+    }
+
+    return () => {
+      if (gameContract) {
+        console.log(gameContract)
+        gameContract.off("CharacterNFTMinted", onCharacterMint);
+      }
+    };
+  }, [gameContract]);
+
+  const renderCharacters = () =>
+    characters.map((character, index) => (
+      <div className="character-item" key={character.name}>
+        <div className="name-container">
+          <p>{character.name}</p>
+        </div>
+        <img src={character.imageURI} alt={character.name} />
+        <button
+          type="button"
+          className="character-mint-button"
+          onClick={mintCharacterNFTAction(index)}
+        >{`Mintar ${character.name}`}</button>
+      </div>
+    ));
+
+  return (
+    <div className="select-character-container">
+      <h2>Minte seu herói. Escolha com sabedoria</h2>
+      {characters.length > 0 && (
+        <div className="character-grid">{renderCharacters()}</div>
+      )}
+      {mintingCharacter && (
+        <div className="loading">
+          <div className="indicator">
+            <LoadingIndicator />
+            <p>Mintando personagem...</p>
+          </div>
+          <img
+            src="https://i.imgur.com/dODaFhu.jpeg"
+            alt="Indicador de Mintagem"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SelectCharacter;
---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0xeF08A4dCa1e8D6E223a5ab4f13C6fdCB97A57C00";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
